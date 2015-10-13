eb97718faf02afcb3f66e22fd04ec632a8e70ba1
Aaron Filson <aaronfilson@gmail.com>
2015-10-13 00:15:29 -0700

    Basic functions of showing images done, adds to votes.

--- app.js
+++ app.js
@@ -18,31 +18,110 @@ var imgFileLocations = [
 "img/PIA18083-800x600.jpg",
 "img/PIA19803-800x600.jpg",
 "img/PSP_008579_9020_descent_800-600.jpg",
-"img/filenamelist.txt",
-"img/glacialcraters_mro.jpg",
-]
+"img/glacialcraters_mro.jpg"
+];
+var didReset = true;
 
-var voteTracker = function () {
+var imgObj = function (pImgFileLoc){
+this.fileNameForImg = pImgFileLoc;
+this.numberOfVotes = null;
+};
+//arg
+var whichSitesNow = [0 , 1];
+var imgArray = [];
+for (var i = 0; i < imgFileLocations.length; i++) {
+  imgArray[i] = new imgObj(imgFileLocations[i]);
+}
 
-  this.imgArray = [];
-  for (var i = 0; i < imgFileLocations.length; i++) {
-    this.imgArray[i] = new imgObj(imgFileLocations[i]);
-  }
-  this.randomPickTwo = function () {
-    var j = Math.ceil(Math.random()*imgFileLocations.length);
+function voteTracker () {
+  this.findTagOne = document.getElementById('clickOne');
+  this.findTagTwo = document.getElementById('clickTwo');
+  this.resetP = document.getElementById('resetP');
+
+  this.randomPickTwo = function ( ) {
+    var j = Math.ceil(Math.random()*(imgFileLocations.length - 1));
+    var k;
     do{
       var loop = false;
-      if (Math.floor(Math.random()*imgFileLocations.length) == j) {
+      if ((k = (Math.floor(Math.random()*(imgFileLocations.length - 1)))) == j) {
         loop = true; //if they match, loop and try again.
       } else {
         return [j, k]; //return the two indexes of random sites to land at.
       }
     } while (loop);
-  }
+  };
+
+
+
+  this.displayImg = function () {
+    whichSitesNow = this.randomPickTwo();
+    this.point1 = this.findTagOne.appendChild(document.createElement('div'));
+    this.point2 = this.findTagTwo.appendChild(document.createElement('div'));
+    console.log('this is where images should get sent to page. index is :' + whichSitesNow[0] + ", " + whichSitesNow[1]);
+    this.point1.innerHTML = "<img src=\'" + imgArray[whichSitesNow[0]].fileNameForImg + "\' alt='The first site to consider' title='This is the first landing site to consider for a mission to Mars. Click on the image to vote in favor.' \>";
+
+    this.point2.innerHTML = "<img src=\'" + imgArray[whichSitesNow[1]].fileNameForImg
+     + "\' alt='The second site to consider' title='This is the second landing site to consider for a mission to Mars. Click on the image to vote in favor.' \>";
+  this.point1.addEventListener('click', this.handleClickOnOne);
+this.point2.addEventListener('click', this.handleClickOnTwo);
+
+
+  };
+
+  this.displayImg();
+
+  this.handleClickOnOne = function (event){
+    console.log('in the handleClickOnOne - ');
+    //add to the counter for the landing site
+    var ser = whichSitesNow[0];
+    if (didReset) {
+      imgArray[ser].numberOfVotes++;
+      didReset = false;
+    }
+
+    console.log('votes is : ' + imgArray[ser].numberOfVotes);
+
+    //highlight the image
+
+
+  };
+
+  this.handleClickOnTwo = function (event){
+
+
+    console.log('in the handleClickOnTwo - ');
+    var ser = whichSitesNow[1];
+
+    //add to the counter for the landing site
+    if (didReset) {
+      imgArray[ser].numberOfVotes++;
+      didReset = false;
+    }
+    console.log('votes is : ' + imgArray[ser].numberOfVotes);
+
+    //highlight the site that was voted for
+
+  };
+
+
+};
+
+function handleTheReset (event) {
+  //call the display function
+  console.log('should now reset');
+  didReset = true;
+  document.getElementById('clickOne').innerHTML = null;
+  document.getElementById('clickTwo').innerHTML = null;
+  pageOneTracker.displayImg();
+
 }
 
-var imgObj = function (pImgFileLoc){
-this.fileNameForImg = pImgFileLoc;
-this.numberOfVotes = 0;
 
-}
+//make the object, it calls all of the other constructors
+var pageOneTracker = new voteTracker();
+
+//attach the listeners
+pageOneTracker.point1.addEventListener('click', pageOneTracker.handleClickOnOne);
+pageOneTracker.point2.addEventListener('click', pageOneTracker.handleClickOnTwo);
+pageOneTracker.resetP.addEventListener('click', handleTheReset);
+
