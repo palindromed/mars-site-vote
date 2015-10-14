af5aaedaacc154347bb15e13ea2334d56a9f6b30
Aaron Filson <aaronfilson@gmail.com>
2015-10-13 23:28:15 -0700

    Chart is working, removed debug messages.

--- README.md
+++ README.md
@@ -15,6 +15,10 @@ Update 3: Starting on chart. Added prototyping. Refactored click handler.
 
 Update 4: Working on chart, using default styles.
 
+Update 5: Tested performance, checked logic, cleaned up code style, removed console
+logging.
+
+
 
 
 Assignment description for Monday, October 12.
--- app.js
+++ app.js
@@ -4,6 +4,10 @@
 
 //The first version of the project will have the user of the site click
 //on an image to vote for where to land a mission to Mars.
+var ImgObj = function (pImgFileLoc){
+  this.fileNameForImg = pImgFileLoc;
+  this.numberOfVotes = 0;
+};
 
 var imgFileLocations = [
 "img/658668main_pia15685-full_full.jpg",
@@ -21,13 +25,8 @@ var imgFileLocations = [
 "img/PSP_008579_9020_descent_800-600.jpg",
 "img/glacialcraters_mro.jpg"
 ];
+//gobal vars etc
 var didReset = true;
-
-var ImgObj = function (pImgFileLoc){
-  this.fileNameForImg = pImgFileLoc;
-  this.numberOfVotes = 0;
-};
-
 var whichSitesNow = [0 , 1];
 var imgArray = [];
 for (var i = 0; i < imgFileLocations.length; i++) {
@@ -43,36 +42,28 @@ var VoteTracker = function() {
     whichSitesNow = this.randomPickTwo();
     this.point1 = this.findTagOne.appendChild(document.createElement('div'));
     this.point2 = this.findTagTwo.appendChild(document.createElement('div'));
-    console.log('this is where images should get sent to page. index is :' + whichSitesNow[0] + ", " + whichSitesNow[1]);
     this.point1.innerHTML = "<img src=\'" + imgArray[whichSitesNow[0]].fileNameForImg + "\' alt='The first site to consider' title='This is the first landing site to consider for a mission to Mars. Click on the image to vote in favor.' \>";
-
-    this.point2.innerHTML = "<img src=\'" + imgArray[whichSitesNow[1]].fileNameForImg
-     + "\' alt='The second site to consider' title='This is the second landing site to consider for a mission to Mars. Click on the image to vote in favor.' \>";
+    this.point2.innerHTML = "<img src=\'" + imgArray[whichSitesNow[1]].fileNameForImg + "\' alt='The second site to consider' title='This is the second landing site to consider for a mission to Mars. Click on the image to vote in favor.' \>";
     this.point1.addEventListener('click', this.handleImgClicks);
     this.point2.addEventListener('click', this.handleImgClicks);
 
   };
-  //call the method right after it is declared.
   this.displayImg();
 };
 
-//chart section
-VoteTracker.prototype.makeChart = function (smoke) {
-  var fire = smoke;
-  console.log(fire);
+VoteTracker.prototype.makeChart = function () {
   var ctx = document.getElementById('voteChart').getContext("2d");
-  var data = {
+  var data = { //beware, hardcoded to 14 landing sites
     labels: ['site1', 'site2', 'site3', 'site4', 'site5', 'site6', 'site7', 'site8','site9', 'site10', 'site11', 'site12', 'site13', 'site14'],
     datasets: [
-    {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: []
-        },
-
+      {
+        label: "Voting Set One, Round One",
+        fillColor: "rgba(220,220,220,0.5)",
+        strokeColor: "rgba(220,220,220,0.8)",
+        highlightFill: "rgba(220,220,220,0.75)",
+        highlightStroke: "rgba(220,220,220,1)",
+        data: []
+      },
     ]
   };
   var outgoing = this.populateChartData(data);
@@ -87,9 +78,7 @@ VoteTracker.prototype.populateChartData = function (pData) {
   return pData;
 }
 
-//clicks on img go here, check which one is target, flip didReset and ++vote.
 VoteTracker.prototype.handleImgClicks = function (event) {
-  console.log('in handleImgClicks method -');
   var tagHandle = (event.currentTarget.parentElement.id == 'clickOne') ? 0 : 1;
   var ser = whichSitesNow[tagHandle];
   if(didReset) {
@@ -97,7 +86,6 @@ VoteTracker.prototype.handleImgClicks = function (event) {
     didReset = false;
     event.currentTarget.parentElement.className = 'highlight';
   }
-  console.log('votes of clicked img is : ' + imgArray[ser].numberOfVotes);
   raiseTheChartFlag();
 };
 
@@ -115,8 +103,6 @@ VoteTracker.prototype.randomPickTwo = function () {
 };
 
 function handleTheReset (event) {
-  //call the display function
-  console.log('should now reset');
   didReset = true;
   document.getElementById('clickOne').innerHTML = null;
   document.getElementById('clickOne').className = null;
@@ -126,17 +112,13 @@ function handleTheReset (event) {
   pageOneTracker.displayImg();
 }
 
-//silly loop because I don't want to deal with scope issues anymore
 function raiseTheChartFlag() {
-  console.log('time for chart');
-  pageOneTracker.makeChart('MOOOOOOOO');
+  pageOneTracker.makeChart();
 }
 
 //make the object, it calls all of the other constructors
 var pageOneTracker = new VoteTracker();
 
-//attach the listeners
 pageOneTracker.point1.addEventListener('click', pageOneTracker.handleImgClicks);
 pageOneTracker.point2.addEventListener('click', pageOneTracker.handleImgClicks);
 pageOneTracker.resetP.addEventListener('click', handleTheReset);
-
